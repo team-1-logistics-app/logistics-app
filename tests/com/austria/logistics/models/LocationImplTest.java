1fb211f6637333f7d5e86b40477a6b9759c43f61
Aleksandar Oynakov <sofia88paradise@gmail.com>
2026-01-30 15:51:55 +0200

    Added tests for LocationImpl

---
+++ tests/com/austria/logistics/models/LocationImplTest.java
@@ -0,0 +1,29 @@
+package com.austria.logistics.models;
+
+import com.austria.logistics.models.enums.Locations;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+
+class LocationImplTest {
+
+    @Test
+    public void constructor_WithSingeArgument_Should_InitializeLocation_When_ArgumentsAreValid() {
+        //Arrange,Act,Assert
+        Assertions.assertEquals(Locations.DAR, new LocationImpl(Locations.DAR).getLocation());
+    }
+
+    @Test
+    public void constructor_WithTwoArguments_Should_InitializeLocation_When_ArgumentsAreValid() {
+        //Arrange
+        LocalDateTime fixedTime = LocalDateTime.now();
+        // Act,Assert
+        Assertions.assertAll(
+                () ->  Assertions.assertEquals(Locations.DAR, new LocationImpl(Locations.DAR, fixedTime).getLocation()),
+                () ->  Assertions.assertEquals(fixedTime, new LocationImpl(Locations.DAR, fixedTime).getEventTime())
+        );
+    }
+
+}
