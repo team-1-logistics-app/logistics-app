50812d18ab397bbfef4b64e8dc9b23fc3ca48706
Aleksandar Oynakov <sofia88paradise@gmail.com>
2026-02-01 12:48:31 +0200

    Added enum class for the truck types

---
+++ src/com/austria/logistics/models/enums/TruckType.java
@@ -0,0 +1,23 @@
+package com.austria.logistics.models.enums;
+
+public enum TruckType {
+    SCANIA("Scania", 42000, 8000),
+    MAN("Man", 37000,10000),
+    ACTROS("Actros", 26000, 13000);
+
+    private final String displayName;
+    private final int capacity;
+    private final int maxRange;
+
+    TruckType(String displayName, int capacity, int maxRange){
+        this.displayName = displayName;
+        this.capacity = capacity;
+        this.maxRange = maxRange;
+    }
+
+    public String getDisplayName() {return this.displayName;}
+    public int getCapacity() {return this.capacity;}
+    public int getMaxRange() {return this.maxRange;}
+
+
+}
