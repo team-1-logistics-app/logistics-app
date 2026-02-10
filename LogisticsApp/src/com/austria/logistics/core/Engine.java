e24fa71e297c8364a6d464a1dc7a4ba75c87ce9e
Aleksandar Oynakov <sofia88paradise@gmail.com>
2026-02-10 22:28:11 +0200

    Added engine functionality

--- LogisticsApp/src/com/austria/logistics/core/Engine.java
+++
@@ -1,5 +0,0 @@
-package com.austria.logistics.core;
-
-public class Engine {
-    //TO DO
-}
---
+++ LogisticsApp/src/com/austria/logistics/core/EngineImpl.java
@@ -0,0 +1,8 @@
+package com.austria.logistics.core;
+
+import com.austria.logistics.core.contracts.CommandFactory;
+import com.austria.logistics.core.contracts.Repository;
+
+public class Engine {
+    
+}
